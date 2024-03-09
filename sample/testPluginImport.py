aba32c60e0586d6fe9ac7f8f882b3dfce1fa53ac
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2024-03-09 14:36:11 +0100

    fix: remove sample

--- sample/testPluginImport.py
+++
@@ -1,9 +0,0 @@
-# importing module.py file
-import importlib 
-from lemniscat.core.util.helpers import LogUtil
-from importlib import resources as impresources
-  
-logger = LogUtil.create()
-pkg = importlib.import_module('lemniscat.plugin.terraform')
-myClass = getattr(pkg, 'Action')
-myClass(logger).info()
